41af88258ab380aabcfe68a78b82ce5a276b17b8
Drew Seeholzer <drews21f7@gmail.com>
2019-06-26 12:05:03 -0600

    Filled out Character info view controller

--- RickandMorty/View Controllers/CharacterInfoViewController.swift
+++ RickandMorty/View Controllers/CharacterInfoViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class CharacterInfoViewController: UIViewController {
 
+    @IBOutlet weak var characterSearchBar: UISearchBar!
+    @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var characterNameLabel: UILabel!
+    @IBOutlet weak var characterSpeciesLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //CharacterController.sharedInstance.fetchCharacterWith(searchTerm: "2") { (success) in
             
-        }
+        
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func isAliveButtonTapped(_ sender: Any) {
+    }
+    
+    func updatesView(with character: RMcharacter) {
+        DispatchQueue.main.async {
+            self.characterNameLabel.text = character.name
+            self.characterSpeciesLabel.text = character.species
+        }
+    }
     /*
     // MARK: - Navigation
 
@@ -30,4 +42,12 @@ class CharacterInfoViewController: UIViewController {
     }
     */
 
+}
+extension CharacterInfoViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        CharacterController.sharedInstance.fetchCharacterWith(searchTerm: searchText) { (character) in
+            guard let character = character else { return }
+            self.updatesView(with: character)
+        }
+    }
 }
