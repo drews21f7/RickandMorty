1354698dd18a005a2af45fbf3362812f685d8325
Drew Seeholzer <drews21f7@gmail.com>
2019-06-26 10:47:21 -0600

    Filled out Model

---
+++ RickandMorty/Models/Character.swift
@@ -0,0 +1,16 @@
+//
+//  Character.swift
+//  RickandMorty
+//
+//  Created by Drew Seeholzer on 6/26/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import Foundation
+
+struct Characters: Decodable {
+    let id: Int
+    let name: String
+    let species: String
+    let status: Bool
+}
