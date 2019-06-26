e8568d7957fd01ec3dbdf05c57729c1e026ce04f
Drew Seeholzer <drews21f7@gmail.com>
2019-06-26 10:28:55 -0600

    Initial Commit

---
+++ RickandMorty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickandMorty
+//
+//  Created by Drew Seeholzer on 6/26/19.
+//  Copyright © 2019 Drew Seeholzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
